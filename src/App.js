d67f09c2bd97a5b97ca2597bb2c25cf42db93cb8
rohitmehta6 <iamakarenite@gmail.com>
2021-06-26 00:56:32 +0530

    Accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { questions } from './api.js';
+import './index.css';
+import Accordion from './Accordion';
 
-function App() {
+const App = () => {
+
+  // const [data, setData] = useState(questions);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="main-div">
+        <h1>React Interview Questions</h1>
+        {
+          questions.map((curElem) => {
+            return <Accordion key={curElem.id}
+              question={curElem.question}
+              answer={curElem.answer}></Accordion>
+          })
+        }
+      </div>
+
+    </>
+  )
 }
 
-export default App;
+export default App
